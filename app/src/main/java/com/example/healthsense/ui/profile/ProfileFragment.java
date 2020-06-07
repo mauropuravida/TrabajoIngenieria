ea7e4f71be49ab6cb5ea416f9a54092dc0f20b55
Mauro <mauropuravida@gmail.com>
2020-06-06 21:15:05 -0300

    Trabajo sobre la vista de perfil de medico
    Se implementaron algunas consultas y gestion de datos para la carga de pais, provincia ciudad, se cambió diseño de spinners

--- app/src/main/java/com/example/healthsense/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/healthsense/ui/profile/ProfileFragment.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -17,6 +19,7 @@ import androidx.fragment.app.Fragment;
 
 import com.example.healthsense.R;
 import com.example.healthsense.Resquest.OkHttpRequest;
+import com.example.healthsense.Resquest.doAsync;
 import com.example.healthsense.data.PikerDate;
 import com.example.healthsense.ui.login.LoginActivity;
 
@@ -37,6 +40,10 @@ import static com.example.healthsense.MainActivity.PREFS_FILENAME;
 public class ProfileFragment extends Fragment {
 
     private Context cont;
+    private ArrayList<String> idsCountries;
+    private ArrayList<String> idsStates;
+    private ArrayList<String> idsCities;
+    private static boolean lock = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -45,31 +52,32 @@ public class ProfileFragment extends Fragment {
         int profile = preferencesEditor.getInt("profileType", R.layout.fragment_profile_medical);
 
         View root = inflater.inflate(profile, container, false);
-
-        Spinner credentialType = root.findViewById(R.id.credential_type);
-        String[] credentialTypes = new String[]{"DNI", "OTRO"};
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(root.getContext(),
-                R.layout.spinner_color, credentialTypes);
-        arrayAdapter.setDropDownViewResource(R.layout.spinner_color);
-        credentialType.setAdapter(arrayAdapter);
-
-        Spinner city = root.findViewById(R.id.city);
-        String[] cities = new String[]{"Tandil", "Necochea", "Azul", "Mal del plata", "Miramar"};
-        ArrayAdapter<String> arrayAdapter3 = new ArrayAdapter<>(root.getContext(),
-                R.layout.spinner_color, cities);
-        arrayAdapter3.setDropDownViewResource(R.layout.spinner_color);
-        city.setAdapter(arrayAdapter3);
-
-        TextView birthDate = root.findViewById(R.id.birth_date);
-
         cont = root.getContext();
 
-        birthDate.setOnClickListener(new View.OnClickListener() {
+        inicSpinner(R.id.country,root,"https://healthsenseapi.herokuapp.com/country/",idsCountries = new ArrayList<>());
+
+        Spinner sp = root.findViewById(R.id.country);
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                new PikerDate(cont).obtenerFecha(birthDate);
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                inicSpinner(R.id.state, root, "https://healthsenseapi.herokuapp.com/state/country_id&" + idsCountries.get((int) sp.getSelectedItemId()), idsStates = new ArrayList<>());
             }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
         });
+
+        Spinner spState = root.findViewById(R.id.state);
+        spState.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                inicSpinner(R.id.city, root, "https://healthsenseapi.herokuapp.com/city/state_id&" + idsStates.get((int) spState.getSelectedItemId()), null);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
 
         final String[] select_qualification = {
                 "Languages","Spanish", "Italian", "English", "Turkish"};
@@ -88,7 +96,7 @@ public class ProfileFragment extends Fragment {
         spinner.setAdapter(myAdapter);
 
         if (profile == R.layout.fragment_profile_medical) {
-            inicMedicalUser(root);
+            inicSpinner(R.id.interal_medicine,root,"https://healthsenseapi.herokuapp.com/medicalspeciality/", null);
         } else {
             inicUser(root);
         }
@@ -100,16 +108,17 @@ public class ProfileFragment extends Fragment {
 
     }
 
-    private void inicMedicalUser(View root){
-        Spinner interarlMedicine = root.findViewById(R.id.interal_medicine);
-        String[] interarlMedicines = new String[]{};
+    private void inicSpinner(int resourse ,View root, String path, ArrayList arr){
+        lock = true;
+        Spinner sp = root.findViewById(resourse);
+        ArrayList<String> values = new ArrayList();
         ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<>(root.getContext(),
-                R.layout.spinner_color, interarlMedicines);
+                R.layout.spinner_color, values);
         arrayAdapter2.setDropDownViewResource(R.layout.spinner_color);
-        interarlMedicine.setAdapter(arrayAdapter2);
+        sp.setAdapter(arrayAdapter2);
 
         OkHttpRequest request = new OkHttpRequest(new OkHttpClient());
-        String conexion = "https://healthsenseapi.herokuapp.com/medicalspeciality/";
+        String conexion = path;
 
         request.GET(conexion, new Callback(){
             @Override
@@ -124,8 +133,8 @@ public class ProfileFragment extends Fragment {
                     String responseData = response.body().string();
                     JSONArray jsonArray = new JSONArray(responseData);
 
-                    Spinner medicalSpeciality = root.findViewById(R.id.interal_medicine);
-                    ArrayList<String> medicalSpecialities = new ArrayList();
+                    Spinner sp = root.findViewById(resourse);
+                    ArrayList<String> values = new ArrayList();
 
                     for (int i=0;i< jsonArray.length(); i++){
 
@@ -134,7 +143,10 @@ public class ProfileFragment extends Fragment {
                             @Override
                             public void run() {
                                 try {
-                                    medicalSpecialities.add(json.getString("name"));
+                                    values.add(json.getString("name"));
+                                    if (arr != null){
+                                        arr.add(json.getString("id"));
+                                    }
                                 } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
@@ -146,24 +158,26 @@ public class ProfileFragment extends Fragment {
                         @Override
                         public void run() {
                             ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(root.getContext(),
-                                    R.layout.spinner_color, medicalSpecialities);
+                                    R.layout.spinner_color, values);
                             arrayAdapter.setDropDownViewResource(R.layout.spinner_color);
-                            medicalSpeciality.setAdapter(arrayAdapter);
+                            sp.setAdapter(arrayAdapter);
                         }
                     });
 
                 } catch (IOException e) {
+                    e.printStackTrace();
                     Looper.prepare();
                     Toast.makeText(root.getContext(), LoginActivity.error(cont,response.code()), Toast.LENGTH_SHORT).show();
                     Looper.loop();
                 } catch (JSONException e) {
+                    e.printStackTrace();
                     Looper.prepare();
                     Toast.makeText(root.getContext(), LoginActivity.error(cont,response.code()), Toast.LENGTH_SHORT).show();
                     Looper.loop();
+                }finally {
+                    lock= false;
                 }
             }
-
         });
-
     }
 }
